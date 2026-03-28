6d10096540898b88f06d9c1aa068ede9f71ac53e
YomnaMajzoub <yomnamajzoub@gmail.com>
2026-03-29 00:04:13 +0300

    ubdate nav bar

--- lib/features/main/presentation/widgets/nav_bar_painter.dart
+++ lib/features/main/presentation/widgets/nav_bar_painter.dart
@@ -2,29 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:servixass/core/constants/app_colors.dart';
 
 class NavBarPainter extends CustomPainter {
-
   final double fabRadius;
 
   NavBarPainter({this.fabRadius = 32});
 
   @override
   void paint(Canvas canvas, Size size) {
-
     final paint = Paint()
       ..color = AppColors.white
       ..style = PaintingStyle.fill;
 
     final borderPaint = Paint()
-      ..color = AppColors.grey50
+      ..color = AppColors.black.withValues(alpha: .18)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 1;
 
     final path = Path();
 
-    final centerX = size.width /2;
+    final centerX = size.width / 2;
 
     /// بداية التجويف
-    final notchStart = centerX - fabRadius -12;
+    final notchStart = centerX - fabRadius - 12;
 
     /// نهاية التجويف
     final notchEnd = centerX + fabRadius + 12;
@@ -32,30 +30,20 @@ class NavBarPainter extends CustomPainter {
     path.moveTo(0, 0);
 
     /// الخط الأيسر
-    path.lineTo(notchStart,0);
+    path.lineTo(notchStart, 0);
 
     /// نزول تدريجي
-    path.quadraticBezierTo(
-      notchStart + 6,
-      0,
-      notchStart + 10,
-      10,
-    );
+    path.quadraticBezierTo(notchStart + 6, 0, notchStart + 12, 12);
 
     /// القوس حول زر FAB
     path.arcToPoint(
-      Offset(notchEnd - 10, 10),
-      radius: Radius.circular(fabRadius +4),
+      Offset(notchEnd - 12, 12),
+      radius: Radius.circular(fabRadius + 4),
       clockwise: false,
     );
 
     /// صعود تدريجي
-    path.quadraticBezierTo(
-      notchEnd - 4,
-      0,
-      notchEnd,
-      0,
-    );
+    path.quadraticBezierTo(notchEnd - 6, 0, notchEnd, 0);
 
     /// الخط الأيمن
     path.lineTo(size.width, 0);
@@ -66,12 +54,7 @@ class NavBarPainter extends CustomPainter {
     path.close();
 
     /// shadow
-    canvas.drawShadow(
-      path,
-      AppColors.grey600,
-      14,
-      true,
-    );
+    canvas.drawShadow(path, AppColors.black, 12, false);
 
     /// background
     canvas.drawPath(path, paint);
